aaa7a2c326f62cef4d942d94aae0726a1da2bf16
jacobbustamante <jacobandrewbustamante@gmail.com>
2015-02-25 13:32:29 -0800

    small changes to enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,7 +11,8 @@ public class Enemy : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		target = GameObject.FindGameObjectWithTag ("Cage").transform;
-		rb2D = GetComponent <Rigidbody2D> ();
+		rb2D = GetComponent <Rigidbody2D> (); // can get rid of?
+		MoveSimple();
 	}
 	
 	// Update is called once per frame
@@ -20,7 +21,11 @@ public class Enemy : MonoBehaviour {
 	}
 	
 	void MoveSimple() {
-		Vector3 newPostion = Vector3.MoveTowards(rb2D.position, target.position, movementSpeed);
-		rb2D.MovePosition (newPostion);
+//		Vector3 newPostion = Vector3.MoveTowards(rb2D.position, target.position, movementSpeed);
+		//rb2D.MovePosition (newPostion);
+		
+		Vector3 direction = target.rigidbody2D.position - this.rigidbody2D.position;
+		Vector3 velocity = direction.normalized * movementSpeed;
+		rigidbody2D.velocity = velocity;
 	}
 }
