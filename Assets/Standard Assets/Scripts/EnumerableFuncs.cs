99ba955963b245386fb73458be53a81540e6a379
David Maxson <scnerd@gmail.com>
2015-02-25 15:29:06 -0800

    Added random map generation code, haven't yet tied it into generating actual in-game maps yet

---
+++ Assets/Standard Assets/Scripts/EnumerableFuncs.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Random = System.Random;
+
+namespace Helpers
+{
+    public struct Tuple<T1, T2>
+    {
+        public readonly T1 Item1;
+        public readonly T2 Item2;
+        public Tuple(T1 first, T2 second)
+        {
+            Item1 = first;
+            Item2 = second;
+        }
+    }
+
+    public static class EnumerableFuncs
+    {
+        private static Random rand = new Random();
+
+        private static int? ResolveIndex<T>(this IEnumerable<T> Arr, int? Num)
+        {
+            return Num.HasValue ? Num >= 0 ? Num : Arr.Count() + Num : null;
+        }
+
+        public static IEnumerable<T> Slice<T>(this IEnumerable<T> Arr, int? Start, int? End)
+        {
+            int ResStart = ResolveIndex(Arr, Start) ?? 0;
+            int ResEnd = ResolveIndex(Arr, End) ?? Arr.Count();
+            return Arr.Skip(ResStart).Take(ResEnd - ResStart);
+        }
+
+        public static void AddIf<T>(this IList<T> Lst, T Element, Func<T, bool> Checker)
+        {
+            if (Checker(Element))
+                Lst.Add(Element);
+        }
+
+        public static void AddIfNotIn<T>(this IList<T> Lst, T Element, IEnumerable<T> ExclusionList)
+        {
+            Lst.AddIf(Element, E => !ExclusionList.Contains(E));
+        }
+
+        public static void AddIf<T>(this HashSet<T> Lst, T Element, Func<T, bool> Checker)
+        {
+            if (Checker(Element))
+                Lst.Add(Element);
+        }
+
+        public static void AddIfNotIn<T>(this HashSet<T> Lst, T Element, IEnumerable<T> ExclusionList)
+        {
+            Lst.AddIf(Element, E => !ExclusionList.Contains(E));
+        }
+
+        public static T BoundedGet<T>(this IEnumerable<T> Arr, int Index)
+        {
+            return Arr.ElementAt(Math.Max(0, Math.Min(Arr.Count() - 1, Index)));
+        }
+
+        public static T RandomChoice<T>(this IEnumerable<T> Arr)
+        {
+            return Arr.ElementAt(rand.Next(Arr.Count()));
+        }
+    }
+
+}
---
+++ Assets/Standard Assets/Scripts/MapBuilder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using Helpers;
+
+public class MapBuilder
+{
+    private LevelBuilder[] Levels;
+    private int LevelWidth, LevelHeight;
+
+    public MapBuilder(int NumLevels, int LevelWidth, int LevelHeight)
+    {
+        Levels = new LevelBuilder[NumLevels];
+        for (int i = 0; i < NumLevels; i++)
+            Levels[i] = new LevelBuilder(LevelWidth, LevelHeight);
+
+        this.LevelWidth = LevelWidth;
+        this.LevelHeight = LevelHeight;
+    }
+
+    public string RenderAsString()
+    {
+        string[][] LevelStrs =
+        Levels.Select(lvl => lvl.RenderAsStrings()).ToArray();
+        return
+            "\r\n".Combine(
+                Enumerable.Range(0, LevelHeight)
+                    .Select(i => "".Combine(LevelStrs.Select(lvl_str => lvl_str[i]).ToArray()))
+                    .ToArray());
+    }
+
+    public bool this[int x, int y]
+    {
+        get
+        {
+            int lvl = x / LevelWidth;
+            x = x % LevelWidth;
+            return Levels[lvl][y, x];
+        }
+        set
+        {
+            int lvl = x / LevelWidth;
+            x = x % LevelWidth;
+            Levels[lvl][y, x] = value;
+        }
+    }
+
+    public void PrintToConsole()
+    {
+        //Console.SetCursorPosition(0, 0);
+        Console.WriteLine(RenderAsString());
+    }
+}
