b9a59e9aff50f6adf9b1c6298bd6b2242afe0456
jacobbustamante <jacobandrewbustamante@gmail.com>
2015-02-24 16:10:25 -0800

    test towers

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class TestScript : MonoBehaviour {
 
+	public Collider2D TestSpawn;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown(KeyCode.A)) {
+			transform.position = new Vector3 (rigidbody2D.position.x - 1, rigidbody2D.position.y, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.W)) {
+			transform.position = new Vector3 (rigidbody2D.position.x, rigidbody2D.position.y + 1, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.D)) {
+			transform.position = new Vector3 (rigidbody2D.position.x + 1, rigidbody2D.position.y, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.S)) {
+			transform.position = new Vector3 (rigidbody2D.position.x, rigidbody2D.position.y -1, 0);
+		}
+	}
+	void FixedUpdate(){
+		if (Input.GetKeyDown (KeyCode.J)) {
+			Collider2D attackInstance = Instantiate (TestSpawn, transform.position + new Vector3 (1, 0, 0), Quaternion.Euler (new Vector3 (0, 0, 0))) as Collider2D;        
+		}
 	}
 }
---
+++ Assets/TestTowerScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestTowerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, 0.7777777f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody2D.velocity = new Vector3(0, 5, 0);
+	}
+}
