f6664e4c3c87d11e0a36aeb41ddd87a74bde5e6e
Michael Murphy <murphym18@gmail.com>
2015-03-05 02:04:18 -0800

    Setup Tyrem's movement input

---
+++ Assets/Scripts/Character/Tyrem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+// The GameObject requires a RigidBody component
+
+[RequireComponent (typeof (SpriteRenderer))]
+[RequireComponent (typeof (Rigidbody2D))]
+[RequireComponent (typeof (BoxCollider2D))]
+public class Tyrem : Character {
+	
+	public float maxSeed = 5f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+		rigidbody2D.velocity = (new Vector2 (h, v)).normalized * maxSeed;
+	}
+}
