989698fe40429dd4d37c2a59a1647ee58d0ea5c2
Nathan Farnum <njfarnum@pcp133588pcs.wireless.calpoly.edu>
2015-03-11 14:18:28 -0700

    Meta picture stuff

---
+++ Assets/Scripts/Character/KnightBulletScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+public class KnightBulletScript : BulletScript {
+	Collider2D[] enemies;
+	float enemyToAttackDistance = 3.0f;
+	float lastAttack = 0.0f;
+	int enemyToAttackIndex = 0;
+	float attackRadius = 3.0f;
+	public LayerMask whatIsTargetable;
+	int hitsLeft = 3;
+	GameObject lastHitEnemy = null;
+
+	KnightBulletScript() {
+		damage = 5.0f;
+		speed = 10.0f;
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Enemy")
+		{
+			// stop and attack
+			//rigidbody2D.velocity = Vector3.zero;
+
+		}
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (ThingsToDieOn.Contains(coll.gameObject.tag) && coll.gameObject.tag == "Enemy")
+		{
+			// stop and attack
+			
+			if(hitsLeft > 0) {
+				enemies = Physics2D.OverlapCircleAll(rigidbody2D.position, attackRadius, whatIsTargetable, 0, 0);
+				enemyToAttackIndex = -1;
+				enemyToAttackDistance = attackRadius;
+				for (int i = 0; enemies != null && i < enemies.Length; i++) {
+					if((enemies[i].transform.position - this.transform.position).magnitude < enemyToAttackDistance && enemies[i].gameObject != lastHitEnemy){
+						enemyToAttackIndex = i;
+						enemyToAttackDistance = (enemies[i].transform.position - this.transform.position).magnitude;
+					}
+				}
+
+				if(enemyToAttackIndex != -1) {
+					lastHitEnemy = enemies[enemyToAttackIndex].gameObject;
+					rigidbody2D.velocity = (enemies[enemyToAttackIndex].transform.position - this.transform.position).normalized*speed;
+					hitsLeft--;
+				}
+			} else {
+				Destroy(gameObject, 0.0f);
+				Debug.Log("Bullet gone\n");
+			}
+		} else if (ThingsToDieOn.Contains(coll.gameObject.tag) && coll.gameObject.tag != "Enemy") {
+			Destroy(gameObject, 0.0f);
+		}
+	}
+}
--- Assets/TestTowerScript.cs
+++ Assets/TestTowerScript.cs
@@ -1,22 +1,87 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 public class TestTowerScript : MonoBehaviour {
+	
+	
+	public static Dictionary<TowerType, int> UpgradeLevels = new Dictionary<TowerType, int>();
+	static TestTowerScript()
+	{
+		UpgradeLevels [TowerType.Pawn] = 0;
+		UpgradeLevels [TowerType.Knight] = 0;
+		UpgradeLevels [TowerType.Bishop] = 0;
+		UpgradeLevels [TowerType.Rook] = 0;
+		UpgradeLevels [TowerType.King] = 0;
+		UpgradeLevels [TowerType.Queen] = 0;
+	}
+
 	// bool attackEnemy = false;
 	public float attackRadius = 10.0f;
 	public LayerMask whatIsTargetable;
-	public float damage = 1.0f;
+	//public float damage = 1.0f;
 	public float attackDelay = 1.0f;
 
 	GameObject target;
 	Collider2D[] enemies;
-	float enemyToAttack = 10.0f;
+	float enemyToAttackDistance = 10.0f;
 	public Rigidbody2D Bullet;
-	public float bulletSpeed = 3.0f;
+	//public float bulletSpeed = 3.0f;
 	float lastAttack = 0.0f;
 	int enemyToAttackIndex = 0;
+
+	public enum TowerType {
+		Pawn, Knight, Bishop, Rook, King, Queen
+	}
+	public TowerType towerType;
+//	private BulletType[] TowerInputKeys;
+//	private class BulletType {
+//		public string input;
+//		public GameObject Bullet;
+//	}
+
+
 	// Use this for initialization
 	void Start () {
+		switch(towerType){
+		case TowerType.Pawn :
+			attackDelay = 1.0f;
+			attackRadius = 10.0f;
+			break;
+		case TowerType.Knight :
+			attackDelay = 2.5f;
+			attackRadius = 10.0f;
+			break;
+		case TowerType.Bishop :
+			attackDelay = 1.5f;
+			attackRadius = 20.0f;
+			break;
+		case TowerType.Rook :
+			attackDelay = 3.5f;
+			attackRadius = 15.0f;
+			break;
+		case TowerType.King :
+			attackDelay = 1.0f; //Have to see on how quickly the blast radius goes
+			attackRadius = 5.0f;
+			break;
+		case TowerType.Queen :
+			attackDelay = 3.0f;
+			attackRadius = 50.0f;
+			break;
+			
+		}
+
+
+
 		//Destroy(gameObject, 0.7777777f);
+//		BulletType[] = new BulletType[] {
+//			createBulletType("ShootPawnBullet", PawnBullet),
+//			createBulletType("ShootKnightBullet", KnightBullet),
+//			createBulletType("ShootBishopnBullet", BishopBullet),
+//			createBulletType("ShootRookBullet", RookBullet),
+//			createBulletType("ShootKingnBullet", KingBullet),
+//			createBulletType("ShootQueenBullet", QueenBullet)
+//		};
 	}//
 	
 	// Update is called once per frame
@@ -26,22 +91,57 @@ public class TestTowerScript : MonoBehaviour {
 		//Physics2D.OverlapCircleNonAlloc(rigidbody2D.position, attackRadius, enemies, whatIsTargetable, 0, 0);
 		enemies = Physics2D.OverlapCircleAll(rigidbody2D.position, attackRadius, whatIsTargetable, 0, 0);
 		enemyToAttackIndex = -1;
-		enemyToAttack = attackRadius;
+		enemyToAttackDistance = attackRadius;
 		for (int i = 0; enemies != null && i < enemies.Length; i++) {
-			if((enemies[i].transform.position - this.transform.position).magnitude < enemyToAttack){
+			if((enemies[i].transform.position - this.transform.position).magnitude < enemyToAttackDistance){
 				enemyToAttackIndex = i;
-				enemyToAttack = (enemies[i].transform.position - this.transform.position).magnitude;
+				enemyToAttackDistance = (enemies[i].transform.position - this.transform.position).magnitude;
 			}
 		}
 		if (Time.time > lastAttack + attackDelay) {
 			lastAttack = Time.time;
 
 			if(enemyToAttackIndex != -1) {
-				Rigidbody2D bulletInstance = Instantiate (Bullet, transform.position, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
-				bulletInstance.GetComponentInParent<BulletScript>().damage = damage;
-				bulletInstance.velocity = (enemies[enemyToAttackIndex].transform.position - this.transform.position).normalized*bulletSpeed;
+				Rigidbody2D bulletInstance;
+				switch(towerType){
+					case TowerType.Pawn :
+						bulletInstance = Instantiate (Bullet, transform.position, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
+						bulletInstance.velocity = (enemies[enemyToAttackIndex].transform.position - this.transform.position).normalized;
+						break;
+					case TowerType.Knight :
+						bulletInstance = Instantiate (Bullet, transform.position, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
+						bulletInstance.velocity = (enemies[enemyToAttackIndex].transform.position - this.transform.position).normalized;
+						break;
+					case TowerType.Bishop :
+						bulletInstance = Instantiate (Bullet, transform.position, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
+						bulletInstance.velocity = (enemies[enemyToAttackIndex].transform.position - this.transform.position).normalized;
+						break;
+					case TowerType.Rook :
+						bulletInstance = Instantiate (Bullet, transform.position, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
+						bulletInstance.velocity = (enemies[enemyToAttackIndex].transform.position - this.transform.position).normalized;
+						break;
+					case TowerType.King :
+						bulletInstance = Instantiate (Bullet, transform.position, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
+						//bulletInstance.velocity = (enemies[enemyToAttackIndex].transform.position - this.transform.position).normalized*bulletSpeed;
+						break;
+					case TowerType.Queen :
+						bulletInstance = Instantiate (Bullet, transform.position, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
+						bulletInstance.velocity = (enemies[enemyToAttackIndex].transform.position - this.transform.position).normalized;
+						break;
+
+				}
+
+
+
+
 			}
 
 		}
 	}
+//	private BulletType createBulletType(string s, GameObject g) {
+//		BulletType b = new BulletType ();
+//		b.input = s;
+//		b.Bullet = g;
+//		return b;
+//	}
 }
