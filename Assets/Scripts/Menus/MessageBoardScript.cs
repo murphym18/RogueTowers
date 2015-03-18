5acb9158070c478ef1e036ab161f58294b7092bb
Michael Murphy <mmurph18@calpoly.edu>
2015-03-17 21:57:04 -0700

    fixed the scrolling text system

--- Assets/Scripts/Menus/MessageBoardScript.cs
+++ Assets/Scripts/Menus/MessageBoardScript.cs
@@ -2,14 +2,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using System;
+using System.Linq;
 
 public class MessageBoardScript : MonoBehaviour
 {
+	class BoardMessage {
+		public BoardMessage(GameObject g) {
+			this.gameObject = g;
 
+			//this.width = maxX - minX;
+			//this.width = g.GetComponent<RectTransform>().rect.width;
+		}
+		public float maxX {
+			get{
+				Vector3[] corners = new Vector3[4]; 
+				gameObject.GetComponent<RectTransform>().GetWorldCorners(corners);
+				float maxX = corners[0].x;
+				foreach (Vector3 v in corners) {
+					maxX = Math.Max(maxX, v.x);
+				}
+				return maxX;
+			}
+		}
+
+		public float minX {
+			get {
+				Vector3[] corners = new Vector3[4]; 
+				gameObject.GetComponent<RectTransform>().GetWorldCorners(corners);
+				float minX = corners[0].x;
+				foreach (Vector3 v in corners) {
+					minX = Math.Min(minX, v.x);
+				}
+				return minX;
+			}
+		}
+		public GameObject gameObject;
+		public float width;
+	}
     public float fontPercent = 0.8f, scrollSpeed = 0.2f, destroyTime = 60f;
+	public float spacing = 0;
     public GameObject scrollingMessageType;
 
-    private Queue<GameObject> messages = new Queue<GameObject>();
+	private Queue<BoardMessage> messages = new Queue<BoardMessage>();
 
     private Rect rect;
 
@@ -21,21 +56,35 @@ public class MessageBoardScript : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-	    while (messages.Count > 0 && messages.Peek() == null)
-	        messages.Dequeue();
-	    foreach (var obj in messages)
-	    {
-	        obj.transform.position = new Vector2(obj.transform.position.x - scrollSpeed, obj.transform.position.y);
-	    }
+		while (messages.Count > 0 && messages.Peek().maxX  < rect.min.x) {
+			BoardMessage m = messages.Dequeue();
+			Destroy(m.gameObject, 0F);
+		}
+
+		BoardMessage obj;
+		if (messages.Count > 0) {
+			obj = messages.Peek();
+			obj.gameObject.transform.position = new Vector2(obj.gameObject.transform.position.x - scrollSpeed, obj.gameObject.transform.position.y);
+		}
+		int i = 1;
+		while (messages.Count > i && messages.ElementAt(i).minX - messages.ElementAt(i-1).maxX > spacing) {
+			obj = messages.ElementAt(i);
+			obj.gameObject.transform.position = new Vector2(obj.gameObject.transform.position.x - scrollSpeed, obj.gameObject.transform.position.y);
+			i = i + 1;
+		}
+
 	}
 
     public void DisplayMessage(string Msg)
     {
-        var instance = Instantiate(scrollingMessageType, new Vector2(rect.width, this.transform.position.y), Quaternion.identity) as GameObject;
-        instance.GetComponent<RectTransform>().SetParent(this.transform);
-        instance.GetComponent<Text>().text = Msg;
-        instance.GetComponent<Text>().fontSize = (int)(this.rect.height*fontPercent);
-        messages.Enqueue(instance);
-        Destroy(instance, destroyTime);
+		float xPos = this.gameObject.transform.position.x + spacing;
+		var instance = Instantiate (scrollingMessageType, new Vector2 (xPos, this.transform.position.y), Quaternion.identity) as GameObject;
+		instance.GetComponent<RectTransform>().SetParent(this.transform);
+		Text text = instance.GetComponent<Text> ();
+		text.text =  Msg + "\t";
+		text.fontSize = (int)(this.rect.height*fontPercent);
+		//text.cachedTextGenerator.Populate (Msg, text.GetGenerationSettings(Vector2.zero));
+		LayoutRebuilder.MarkLayoutForRebuild (instance.transform as RectTransform);
+        messages.Enqueue(new BoardMessage(instance));
     }
 }
