27bf335d589fb22abe5e46a710a20cb52c6da322
Michael Murphy <mmurph18@calpoly.edu>
2015-03-17 18:06:27 -0700

    added the loading screen

---
+++ Assets/Scripts/Menus/LoadingScreenScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingScreenScript : MonoBehaviour {
+	public string gameWorldLevel = "GeneratedLevel";
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Application.LoadLevel(gameWorldLevel);
+	}
+}
