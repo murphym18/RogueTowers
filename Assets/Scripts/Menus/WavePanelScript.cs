db421a2907bb2b6dbbde4891a467db44dc1cdc22
jacobbustamante <jacobandrewbustamante@gmail.com>
2015-03-18 18:22:06 -0700

    fixed small bug in wave panel

---
+++ Assets/Scripts/Menus/WavePanelScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WavePanelScript : MonoBehaviour {
+
+	public GameObject textObject;
+
+	private WaveManagerScript waveManager;
+	private Text waveText;
+
+	void Awake() {
+		waveManager = GameObject.Find("GameManager").GetComponent<WaveManagerScript>();
+	}
+	
+	void Start () {
+		waveText = textObject.GetComponent<Text>();
+	}
+
+	public void UpdatePanel()
+	{
+		waveText.text = GetWaveString();
+	}
+
+	private string GetWaveString()
+	{
+		string waveNumber = "Wave:\t\t\t\t" + waveManager.waveNumber.ToString();
+		string remaining = "Enemies left:\t" + waveManager.curEnemyCount + "\n";
+		string basic = "\t\t\t\tx" + WaveManagerScript.enemiesRemaining[Enemy.EnemyType.Basic].ToString();
+		string fast = "\t\t\t\tx" + WaveManagerScript.enemiesRemaining[Enemy.EnemyType.Fast].ToString();
+		string strong = "\t\t\t\tx" + WaveManagerScript.enemiesRemaining[Enemy.EnemyType.Strong].ToString();
+
+		string[] arr = new string[] {waveNumber,remaining,basic,fast,strong};
+
+		return string.Join("\n", arr);
+	}
+}
